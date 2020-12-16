9b1fc439ea5ddca077d71dd069716ce1505fc838
Connor Szczepaniak <cszczepaniak@gmail.com>
2020-12-16 00:02:59 -0600

    implement a basic connection to the public and private backend routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Auth0Provider } from '@auth0/auth0-react';
+import { authConfig } from './auth/config';
+import { SampleAuth } from './components/SampleAuth/SampleAuth';
+
 function App() {
   return (
-    <div>
-      Hello, Cribbly!
-    </div>
+    <Auth0Provider
+      domain={authConfig.domain}
+      clientId={authConfig.clientId}
+      audience={authConfig.audience}
+      redirectUri={window.location.origin}
+      scope={authConfig.scope}
+    >
+      <SampleAuth />
+    </Auth0Provider>
   );
 }
 
