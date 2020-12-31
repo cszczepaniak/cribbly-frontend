59158576865ea13efbdeb3f837c28afd25b81b39
Adam Szczepaniak <adam.r.szczepaniak@gmail.com>
2020-12-30 23:13:42 -0600

    add material ui and sample component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import { authConfig } from './auth/config';
 import { SampleAuth } from './components/SampleAuth/SampleAuth';
+import { SampleMaterial } from './components/SampleMaterial';
 
 function App() {
   return (
-    <Auth0Provider
-      domain={authConfig.domain}
-      clientId={authConfig.clientId}
-      audience={authConfig.audience}
-      redirectUri={window.location.origin}
-      scope={authConfig.scope}
-    >
-      <SampleAuth />
-    </Auth0Provider>
+    <div className="main-container">
+      <Auth0Provider
+        domain={authConfig.domain}
+        clientId={authConfig.clientId}
+        audience={authConfig.audience}
+        redirectUri={window.location.origin}
+        scope={authConfig.scope}>
+        <SampleAuth />
+      </Auth0Provider>
+      <SampleMaterial/>
+    </div>
+
+
   );
 }
 
