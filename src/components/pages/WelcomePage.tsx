2c28e181a1ce5b11e7783f392a036530c01d120a
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-01-08 20:32:27 -0600

    simplify some more

--- src/components/pages/WelcomePage.tsx
+++ src/components/pages/WelcomePage.tsx
@@ -3,12 +3,9 @@ import { useAuth } from '../../hooks/useAuth';
 
 export const WelcomePage = () => {
   const { user, signOut } = useAuth();
-  if (!user) {
-    return <div>error</div>;
-  }
   return (
     <div>
-      <div>Hello, {user.displayName}</div>
+      <div>Hello, {user?.displayName}</div>
       <Button color='primary' variant='contained' onClick={signOut}>
         Logout
       </Button>
