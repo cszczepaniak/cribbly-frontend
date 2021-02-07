f80b7bed801e188e35de70fa3694aab4c258c6ec
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-02-07 16:36:55 -0600

    add a theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './components/routing/Routes';
 import { ProvideAuth } from './hooks/useAuth';
 import { ProvideTournament } from './hooks/useTournament';
 
+const theme = createMuiTheme({
+    props: {
+        MuiButton: {
+            variant: 'contained',
+            color: 'primary',
+        },
+    },
+});
+
 function App() {
     return (
-        <ProvideAuth>
-            <ProvideTournament>
-                <BrowserRouter>
-                    <Routes />
-                </BrowserRouter>
-            </ProvideTournament>
-        </ProvideAuth>
+        <ThemeProvider theme={theme}>
+            <ProvideAuth>
+                <ProvideTournament>
+                    <BrowserRouter>
+                        <Routes />
+                    </BrowserRouter>
+                </ProvideTournament>
+            </ProvideAuth>
+        </ThemeProvider>
     );
 }
 
