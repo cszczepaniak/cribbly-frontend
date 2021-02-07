cec0acfd6b6210c9556c65d215ec858da4f6c42b
Connor Szczepaniak <cszczepaniak@gmail.com>
2021-02-07 16:38:48 -0600

    build out the ugliest team page you've ever seen

--- src/components/routing/Routes.tsx
+++ src/components/routing/Routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { LandingPage } from '../pages/LandingPage';
-import { TeamPage } from '../pages/TeamPage';
+import { TeamPage } from '../pages/TeamPage/TeamPage';
 import { WelcomePage } from '../pages/WelcomePage';
 import { ProtectedRoute } from '../routing/ProtectedRoute';
 export const Routes = () => {
